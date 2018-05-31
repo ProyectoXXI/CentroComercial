a52efe013b270a05c393be29abdc585b4732c8e2
Rassa95 <39536503+Rassa95@users.noreply.github.com>
2018-05-31 12:33:13 +0200

    Login y pantallas principales

---
+++ GestorCentroComercial/GestorCentroComercial/HomeDependientes.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GestorCentroComercial
+{
+    partial class HomeDependientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bMostrarNominas = new System.Windows.Forms.Button();
+            this.bMostrarVentas = new System.Windows.Forms.Button();
+            this.bSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bMostrarNominas
+            // 
+            this.bMostrarNominas.Location = new System.Drawing.Point(46, 30);
+            this.bMostrarNominas.Name = "bMostrarNominas";
+            this.bMostrarNominas.Size = new System.Drawing.Size(114, 79);
+            this.bMostrarNominas.TabIndex = 0;
+            this.bMostrarNominas.Text = "Mostrar Nóminas";
+            this.bMostrarNominas.UseVisualStyleBackColor = true;
+            // 
+            // bMostrarVentas
+            // 
+            this.bMostrarVentas.Location = new System.Drawing.Point(235, 30);
+            this.bMostrarVentas.Name = "bMostrarVentas";
+            this.bMostrarVentas.Size = new System.Drawing.Size(117, 79);
+            this.bMostrarVentas.TabIndex = 1;
+            this.bMostrarVentas.Text = "Mostrar Ventas";
+            this.bMostrarVentas.UseVisualStyleBackColor = true;
+            // 
+            // bSalir
+            // 
+            this.bSalir.Location = new System.Drawing.Point(139, 148);
+            this.bSalir.Name = "bSalir";
+            this.bSalir.Size = new System.Drawing.Size(127, 62);
+            this.bSalir.TabIndex = 2;
+            this.bSalir.Text = "Salir";
+            this.bSalir.UseVisualStyleBackColor = true;
+            // 
+            // HomeDependientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 239);
+            this.Controls.Add(this.bSalir);
+            this.Controls.Add(this.bMostrarVentas);
+            this.Controls.Add(this.bMostrarNominas);
+            this.Name = "HomeDependientes";
+            this.Text = "HomeDependientes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bMostrarNominas;
+        private System.Windows.Forms.Button bMostrarVentas;
+        private System.Windows.Forms.Button bSalir;
+    }
+}
---
+++ GestorCentroComercial/GestorCentroComercial/Login.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GestorCentroComercial
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.bLogin = new System.Windows.Forms.Button();
+            this.bExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(76, 48);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(181, 20);
+            this.tbUser.TabIndex = 2;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(72, 126);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(185, 20);
+            this.tbPass.TabIndex = 3;
+            // 
+            // bLogin
+            // 
+            this.bLogin.Location = new System.Drawing.Point(46, 174);
+            this.bLogin.Name = "bLogin";
+            this.bLogin.Size = new System.Drawing.Size(88, 35);
+            this.bLogin.TabIndex = 4;
+            this.bLogin.Text = "Login";
+            this.bLogin.UseVisualStyleBackColor = true;
+            // 
+            // bExit
+            // 
+            this.bExit.Location = new System.Drawing.Point(198, 174);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(97, 34);
+            this.bExit.TabIndex = 5;
+            this.bExit.Text = "Salir";
+            this.bExit.UseVisualStyleBackColor = true;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 231);
+            this.Controls.Add(this.bExit);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button bLogin;
+        private System.Windows.Forms.Button bExit;
+    }
+}
+
