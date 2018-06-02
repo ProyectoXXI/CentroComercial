09b3b88ee4cde63619161b0bebb3e392fa720167
Rassa95 <39536503+Rassa95@users.noreply.github.com>
2018-06-02 19:38:40 +0200

    Login bien hecho.

--- GestorCentroComercial/GestorCentroComercial/Login.Designer.cs
+++ GestorCentroComercial/GestorCentroComercial/Login.Designer.cs
@@ -34,53 +34,46 @@
             this.tbPass = new System.Windows.Forms.TextBox();
             this.bLogin = new System.Windows.Forms.Button();
             this.bExit = new System.Windows.Forms.Button();
-            this.consultas1 = new GestorCentroComercial.Consultas();
             this.empleadoTableAdapter1 = new GestorCentroComercial.ConsultasTableAdapters.EmpleadoTableAdapter();
-            ((System.ComponentModel.ISupportInitialize)(this.consultas1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(184, 28);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(138, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(172, 122);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(129, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña";
             // 
             // tbUser
             // 
-            this.tbUser.Location = new System.Drawing.Point(101, 59);
-            this.tbUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbUser.Location = new System.Drawing.Point(76, 48);
             this.tbUser.Name = "tbUser";
-            this.tbUser.Size = new System.Drawing.Size(240, 22);
+            this.tbUser.Size = new System.Drawing.Size(181, 20);
             this.tbUser.TabIndex = 2;
             // 
             // tbPass
             // 
-            this.tbPass.Location = new System.Drawing.Point(96, 155);
-            this.tbPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbPass.Location = new System.Drawing.Point(72, 126);
             this.tbPass.Name = "tbPass";
-            this.tbPass.Size = new System.Drawing.Size(245, 22);
+            this.tbPass.Size = new System.Drawing.Size(185, 20);
             this.tbPass.TabIndex = 3;
             // 
             // bLogin
             // 
-            this.bLogin.Location = new System.Drawing.Point(61, 214);
-            this.bLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bLogin.Location = new System.Drawing.Point(46, 174);
             this.bLogin.Name = "bLogin";
-            this.bLogin.Size = new System.Drawing.Size(117, 43);
+            this.bLogin.Size = new System.Drawing.Size(88, 35);
             this.bLogin.TabIndex = 4;
             this.bLogin.Text = "Login";
             this.bLogin.UseVisualStyleBackColor = true;
@@ -88,18 +81,12 @@
             // 
             // bExit
             // 
-            this.bExit.Location = new System.Drawing.Point(264, 214);
-            this.bExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bExit.Location = new System.Drawing.Point(198, 174);
             this.bExit.Name = "bExit";
-            this.bExit.Size = new System.Drawing.Size(129, 42);
+            this.bExit.Size = new System.Drawing.Size(97, 34);
             this.bExit.TabIndex = 5;
             this.bExit.Text = "Salir";
             this.bExit.UseVisualStyleBackColor = true;
-            // 
-            // consultas1
-            // 
-            this.consultas1.DataSetName = "Consultas";
-            this.consultas1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // empleadoTableAdapter1
             // 
@@ -107,19 +94,18 @@
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(452, 284);
+            this.ClientSize = new System.Drawing.Size(339, 231);
             this.Controls.Add(this.bExit);
             this.Controls.Add(this.bLogin);
             this.Controls.Add(this.tbPass);
             this.Controls.Add(this.tbUser);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "login";
             this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.consultas1)).EndInit();
+            this.Load += new System.EventHandler(this.login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,7 +119,6 @@
         private System.Windows.Forms.TextBox tbPass;
         private System.Windows.Forms.Button bLogin;
         private System.Windows.Forms.Button bExit;
-        private Consultas consultas1;
         private ConsultasTableAdapters.EmpleadoTableAdapter empleadoTableAdapter1;
     }
 }
--- GestorCentroComercial/GestorCentroComercial/Login.cs
+++ GestorCentroComercial/GestorCentroComercial/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,21 +21,53 @@ namespace GestorCentroComercial
         private void bLogin_Click(object sender, EventArgs e)
         {
             String usuario = tbUser.Text.ToString();
+
             int password = int.Parse(tbPass.Text.ToString());
-            if(empleadoTableAdapter1.ComprobarPassword(password) != null){
-                if (empleadoTableAdapter1.ComprobarUsuario(usuario) != null)
+
+            DataTable consulta = empleadoTableAdapter1.ComprobarLogin(password, usuario);
+
+            if (consulta != null) {
+
+                DataRow empleado = consulta.Rows[0];
+
+                MessageBox.Show(empleado["Rol"].ToString());
+
+                switch (empleado["Rol"].ToString())
                 {
-                    MessageBox.Show("Entra");
+
+                    case "dependiente":
+                        HomeDependientes dependiente = new HomeDependientes();
+                        dependiente.Visible = true;
+                        this.Visible = false;
+
+                        break;
+                    case "director":
+                        HomeDirectores director = new HomeDirectores();
+                        director.Visible = true;
+                        this.Visible = false;
+
+                        break;
+                    case "gerente":
+                        HomeTecnicos tecnico = new HomeTecnicos();
+                        tecnico.Visible = true;
+                        this.Visible = false;
+
+                        break;
                 }
-                else
-                {
-                    MessageBox.Show("Usuario no valido");
-                }
+
+
             }
             else
             {
-                MessageBox.Show("Contraseña no valida");
+                MessageBox.Show("Error en el login.");
             }
+        
+
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
